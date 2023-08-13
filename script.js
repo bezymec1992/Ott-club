42c5ecb7562802346dddf29d34aea6fb525feda5
Vasiliy Lavrov <68432186+bezymec1992@users.noreply.github.com>
2023-08-13 18:22:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 AOS.init();
-AOS.init({once:true, offset: 0});
+AOS.init({ once: true, offset: 0 });
 
 const cards = document.querySelectorAll(".tv__img"),
     see = document.querySelector(".see"),
-    accardions = document.querySelectorAll(".accardion");
+    accardions = document.querySelectorAll(".accardion"),
+    btns = document.querySelectorAll('.tv__btn');
 
 see.addEventListener('click', () => {
     see.classList.toggle('open');
-    for(card of cards) {
-        card.classList.contains('hiden') ? card.classList.toggle("d-none") : '' 
+    for (card of cards) {
+        card.classList.contains('hiden') ? card.classList.toggle("d-none") : ''
     }
 })
 
-for(item of accardions) {
+for (item of accardions) {
     item.addEventListener("click", function () {
         this.classList.toggle("active");
         let panel = this.nextElementSibling;
         panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px";
     })
 }
+
+for (btn of btns) {
+    btn.addEventListener('click', function () {
+        for (button of btns) {
+            button.classList.remove('active')
+        }
+        this.classList.add("active")
+    })
+}
+
